7d44404b0d0389f9d18a76bc2038732d1e592303
if414028 <voldarex.napitupulu@gmail.com>
2026-01-27 16:46:49 +0700

    fix performance margin

--- resources/views/performances/index.blade.php
+++ resources/views/performances/index.blade.php
@@ -1,6 +1,6 @@
 <x-dashboard-layout>
-    <div x-data="teamSheet" class="px-6 py-6">
-        <div class="flex items-start justify-between gap-4 flex-wrap">
+    <div x-data="teamSheet">
+        <div class="flex items-center justify-between gap-4">
             <div>
                 <h1 class="text-2xl font-semibold text-gray-900">My Performance</h1>
                 <p class="text-sm text-gray-500">Monitor performa tim.</p>
